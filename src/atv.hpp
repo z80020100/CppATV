ce36a44eb82e7cff26619dbc39ce5d458b0f213a
Cliff Wu <z800201002005@gmail.com>
2019-06-25 19:07:34 +0800

    Send pair-pin-start request to Apple TV

---
+++ src/atv.hpp
@@ -0,0 +1,1 @@
+#define APPLE_URL_PAIR_PIN_START "/pair-pin-start"
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 
 #include <cstdio>
@@ -7,14 +8,48 @@
 #include <unistd.h>
 
 #include "simpleHttpClient.hpp"
+#include "atv.hpp"
+
+#define BUF_SIZE 4096
 
 using namespace std;
 
+string genStep0Request(char *host, int port) {
+	// POST /pair-pin-start HTTP/1.1
+	// Host: 192.168.0.147:7000
+	// Connection: keep-alive
+	// User-Agent: AirPlay/320.20
+	// Accept-Encoding: gzip, deflate
+	// Accept: */*
+	// Content-Length: 0
+	// Content-Type: application/octet-stream
+
+	ostringstream ostrStep0Request;
+	string UrlStr = APPLE_URL_PAIR_PIN_START;
+
+	ostrStep0Request << HTTP_REQUEST_METHOD_POST << " " << UrlStr << " " << HTTP_PROTOCOL_VERSION << "\r\n";
+	ostrStep0Request << "Host: " << host << ":" << port << "\r\n";
+	ostrStep0Request << "Connection: keep-alive" << "\r\n";
+	ostrStep0Request << "User-Agent: AirPlay/320.20" << "\r\n";
+	ostrStep0Request << "Accept-Encoding: gzip, deflate" << "\r\n";
+	ostrStep0Request << "Accept: */*" << "\r\n";
+	ostrStep0Request << "Content-Length: 0" << "\r\n";
+	ostrStep0Request << "Content-Type: application/octet-stream" << "\r\n";
+	ostrStep0Request << "\r\n";
+
+	return ostrStep0Request.str();
+}
+
 int main() {
+	bool debug = true;
+	int ret = -1;
+
 	char* host = "192.168.0.147";
 	int port = 7000;
+	char recvBuf[BUF_SIZE] = {0};
+
 	SimpleHttpClient httpClient = SimpleHttpClient(host, port);
-	int ret = -1;
+	string strRequestData;
 
 	ret = httpClient.tcpConnect();
 	printf("Connect to %s:%d ", host, port);
@@ -22,6 +57,34 @@ int main() {
 		printf("succeed\n");
 	} else {
 		printf("failed, ret = %d\n", ret);
+		return -1;
+	}
+
+	// Request Apple TV to PIN pair (show PIN on Apple TV screen)
+	strRequestData = genStep0Request(host, port);
+	ret = httpClient.sendData(strRequestData.c_str(), strRequestData.length());
+	if(ret != 1) {
+		printf("Send %d bytes data\n", ret);
+		if(debug) {
+			cout << "################################" << endl;
+			cout << strRequestData << endl;
+			cout << "################################" << endl << endl;
+		}
+	} else {
+		printf("Send data failed, ret = %d\n", ret);
+		return -1;
+	}
+
+	ret = httpClient.recvData(recvBuf, BUF_SIZE);
+	if(ret != 1) {
+		printf("Reveive %d bytes data\n", ret);
+		if(debug) {
+			cout << "################################" << endl;
+			cout << recvBuf << endl;
+			cout << "################################" << endl << endl;
+		}
+	} else {
+		printf("Receive data failed, ret = %d\n", ret);
 		return -1;
 	}
 
