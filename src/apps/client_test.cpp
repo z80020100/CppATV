a5790e36d7bcfeeca55db5bab1b4b9627a1bf196
Cliff Wu <z800201002005@gmail.com>
2019-07-01 17:25:28 +0800

    Modify SRP client N, g to 2048 to fit Apple TV

--- src/apps/client_test.cpp
+++ src/apps/client_test.cpp
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 	try {
 		OsslSha1 hash;
 		OsslRandom random;
-		Ng ng = Ng::predefined(1024);
+		Ng ng = Ng::predefined(2048);
 		OsslMathImpl math(hash, ng);
 		
 		SrpClient srpclient(math, random, false);
